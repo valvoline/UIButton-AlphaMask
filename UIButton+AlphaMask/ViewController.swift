db1c4a6017b1230c79847c89894713c433b3a1ec
valvoline <valvoline@gmail.com>
2017-02-04 09:55:14 +0100

    Initial Commit

---
+++ UIButton+AlphaMask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIButton+AlphaMask
+//
+//  Created by valvoline on 04/02/2017.
+//  Copyright © 2017 sofapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
